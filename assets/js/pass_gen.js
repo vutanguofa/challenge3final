2b41c2cc28eb7b8cc9ab2a457a23e333dd149ff7
vutanguofa <vu.q.tang3@gmail.com>
2020-08-16 14:27:54 -0700

    uploading changes

--- assets/js/pass_gen.js
+++ assets/js/pass_gen.js
@@ -14,16 +14,16 @@ THEN the password is either displayed in an alert or written to the page
 var promptLength = window.prompt("Please enter a maximum length for your password.");
 console.log("Your password will be between 8 to " + promptLength + " characters long.");
 if (promptLength >= 8) {
-    var promptLowercase = window.prompt("Would you like to include lowercase characters? Enter 1 for YES and 2 for NO");
+    var promptLowercase = window.prompt("Would you like to include lowercase characters? Enter 1 for \"YES\" and 2 for \"NO\"");
     console.log("You want to include lowercase characters");
     if (promptLowercase === "1") {
-        var promptUppercase = window.prompt("Would you like to include uppercase characters? Enter 1 for YES and 2 for NO");
+        var promptUppercase = window.prompt("Would you like to include uppercase characters? Enter 1 for \"YES and 2 for \"NO\"");
         console.log("You want to include uppercase characters");
         if (promptUppercase === "1") {
-            var promptNumeric = window.prompt("Would you like to include numeric characters? Enter 1 for YES and 2 for NO");
+            var promptNumeric = window.prompt("Would you like to include numeric characters? Enter 1 for \"YES and 2 for \"NO\"");
             console.log("You want to include numeric characters");
             if (promptNumeric === "1") {
-                var promptSpecial = window.prompt("Would you like to include special characters? Enter 1 for YES and 2 for NO");
+                var promptSpecial = window.prompt("Would you like to include special characters? Enter 1 for \"YES and 2 for \"NO\"");
                 console.log("You want to include special characters");
             }
         }
@@ -34,27 +34,7 @@ else {
 }
 
 function criteria() {
-    console.log(promptLength, promptLowercase, promptUppercase, promptNumeric, promptSpecial);
-    if (promptLength >= 8) {
-        length = Math.ceil(Math.random() * 128);
-        window.alert(length);
-    }
-    if (promptLowercase === 1) {
-        lowercase = console.log();
-        window.alert(promptLowercase);
-    }
-    if (promptUppercase === 1) {
-        uppercase = console.log();
-        window.alert(promptUppercase);
-    }
-    if (promptNumeric === 1) {
-        numeric = console.log();
-        window.alert(promptNumeric);
-    }
-    if (promptSpecial === 1) {
-        special = console.log();
-        window.alert(promptSpecial);
-    }
+    console.log("Password length will be between 8 and " + promptLength + ".\n" + promptLowercase, promptUppercase, promptNumeric, promptSpecial);
 }
 
 criteria();
